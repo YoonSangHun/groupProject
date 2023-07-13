d99232bd45d3f48c6af1605133bfe464d3ff8e30
YoonSangHun <vhffptwkd@naver.com>
2023-07-13 15:53:48 +0900

    test4

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,26 @@
+import {Swiper, SwiperSlide} from "swiper/react";
 import NavBar from "../components/NavBar";
 
 const Home = () => {
     return (
         <main className="container">
-            <NavBar /><hr/>
+            <hr/><NavBar /><hr/>
+            <div className="grid">
+            
+            <Swiper
+              spaceBetween={50}
+              slidesPerView={3}
+              onSlideChange={() => console.log('slide change')}
+              onSwiper={(swiper) => console.log(swiper)}
+            >
+              <SwiperSlide>Slide 1</SwiperSlide>
+              <SwiperSlide>Slide 2</SwiperSlide>
+              <SwiperSlide>Slide 3</SwiperSlide>
+              <SwiperSlide>Slide 4</SwiperSlide>
+              ...
+            </Swiper>
+            </div>
+            
         </main>
     );
 }
