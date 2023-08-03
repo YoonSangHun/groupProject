53362b5c96d2b03377a42b0ce6d22a75cc710d09
DevChild <vhffptwkd@naver.com>
2023-08-03 18:17:53 +0900

    detail's Frame

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -57,41 +57,78 @@ const Detail = () => {
             {/* 아래부터 시각화 이미지 렌더링 */}
             </article><hr/>
             <div className="grid">
+                {/* Logo 위치 */}
                 <article id="logo">
                     <h4 text align="center"><i>About...</i></h4>
                     <img src={logoUrl} alt="logo"/>
                     <footer text align="center"><small><strong><mark>{ company_name }</mark></strong>는요...!</small></footer>  
                 </article>
-                <article id="wordcloud">
-                    <h4 text align="center"><i>Word Cloud...</i></h4>
-                    <img src='img\wordcloudtest3.png' alt="wordcloud"/>
-                    <footer text align="center"><small><strong><mark>이런 장점</mark></strong>이 있어요!</small></footer>  
+                {/* 특징 wordcloud */}
+                <article id="wordcloud1">
+                    <h4 text align="center">한 눈에 보는 이 기업 특징</h4>
+                    <img src='img\wordcloudtest3.png' alt="wordcloud1"/>
+                    <footer text align="center"><small><strong><mark>단어의 크기</mark></strong>가 중요해요!</small></footer>  
                 </article>               
             </div><hr/>
 
             <div className="grid">
-                <article id="genderratio">
-                    <h4 text align="center"><i>Gender Ratio</i></h4>
-                    <img src='img/sgraph.png' alt="genderratio"/>
-                    <footer text align="center"><small>구성원의 <strong><mark>남녀성비</mark></strong>는 어떨까요?</small></footer>                  
+                {/* 장점 wordcloud */}
+                <article id="wordcloud2">
+                    <h4 text align="center">Word Cloud <mark>(장점)</mark></h4>
+                    <img src='img/wordcloudtest3.png' alt="wordcloud2"/>
+                    <footer text align="center"><small>많이 언급된<strong><mark>장점은?</mark></strong></small></footer>                  
                 </article>
-                <article id="incomestate">
-                    <h4 text align="center"><i>Income State</i></h4>
-                    <img src='img\incomestate.png' alt="incomestate"/>
-                    <footer text align="center"><small><strong><mark>재무제표 정보</mark></strong>를 그래프로!</small></footer>                  
+                {/* 단점 wordcloud */}
+                <article id="wordcloud3">
+                    <h4 text align="center">Word Cloud <mark>(단점)</mark></h4>
+                    <img src='img\wordcloudtest3.png' alt="wordcloud3"/>
+                    <footer text align="center"><small>많이 언급된<strong><mark>단점은?</mark></strong></small></footer>                  
                 </article>
             </div><hr/>    
+
             <div className='grid'>
-                <article id="type">
-                        <h4 text align="center"><i>Type...</i></h4>
-                        <img src='img\9.jpg' alt="type"/>
-                        <footer text align="center"><small><strong><mark>ㅇㅇ유형</mark></strong>으로 분류되었어요!</small></footer>  
-                    </article>
-                    <article id="Average Salary">
-                        <h4 text align="center"><i>Average Salary</i></h4>
-                        <img src='img\salary.png' alt="salary"/>
-                        <footer text align="center"><small><strong><mark>평균 연봉</mark></strong>과 업계평균을 비교...?</small></footer>                  
-                    </article>
+                {/* 위치 정보 */}
+                <article id="location">
+                    <h4 text align="center">이 동네에 있는 회사네요!</h4>
+                    <img src='img\map.png' alt="location"/>
+                    <footer text align="center"><small><strong><mark>집 가까운 게</mark>최고죠!</strong></small></footer>  
+                {/* 평균연봉 */}
+                </article>
+                <article id="averagesalary">
+                    <h4 text align="center"><i>Average Salary</i></h4>
+                    <img src='img\salary.png' alt="salary"/>
+                    <footer text align="center"><small><strong>업계 평균은 <mark>빨간색</mark>이 회사는</strong><strong><mark>파란색!</mark></strong></small></footer>                  
+                </article>
+            </div><hr/>
+
+            <div className='grid'>
+                {/* 구성원 성비 */}
+                <article id="genderratio">
+                    <h4 text align="center">구성원의 성비는...?</h4>
+                    <img src='img\sgraph.png' alt="genderratio"/>
+                    <footer text align="center"><small><strong><mark>직원의 성비</mark>어떤 회사가 좋으세요?</strong></small></footer>  
+                {/* 매출추세 */}
+                </article>
+                <article id="salestrend">
+                    <h4 text align="center">연도별 매출을 확인하세요!</h4>
+                    <img src='img\salestrend.png' alt="salestrend"/>
+                    <footer text align="center"><small><strong><mark>연도 별 매출 증감추세</mark></strong>를 확인하실 수 있어요.</small></footer>                  
+                </article>
+            </div><hr/>
+
+            <div className='grid'>
+                {/* 레이더 차트 */}
+                <article id="incomestate">
+                    <h4 text align="center">재무 성향을 레이더로 시각화했어요!</h4>
+                    <img src='img\navien.png' alt="incomestate"/>
+                    <footer text align="center"><small><strong><mark>안정성</mark>은 숫자가 작을수록 긍정적이라는 뜻이에요.</strong></small></footer>  
+                {/* 복지 */}
+                </article>
+                <article id="welfare">
+                    <h4 text align="center">복지만족도에 별점을 매겨봤어요 :)</h4>
+                    <img src='img\welfare.png' alt="welfare"/>
+                    <footer text align="center"><small><strong><mark>워크넷 리뷰데이터</mark>를 참고했어요!</strong></small></footer>                  
+                </article>
             </div><hr/>
                    
             <div className="grid">
