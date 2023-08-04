0ad814c7881bc63fcf1b1b950a1218da2df955a5
jinchang9292 <jinchang9292@gmail.com>
2023-08-04 13:14:30 +0900

    'merging'

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,91 +1,112 @@
-import React, { useState, useEffect } from 'react';
-import NavBar from "../components/NavBar";
+import React, { useState } from "react";
+import axios from 'axios';
+import NavBar from '../components/NavBar';
 
 const Detail = () => {
-    const [searchInput, setSearchInput] = useState(''); // New state variable for the search input
-    const [corp_code, setCorpCode] = useState(''); // State variable for the corp_code
-    const [company_name, setCompanyName] = useState(''); // State variable for the company name
+    const [searchInput, setSearchInput] = useState('');
     const [logoUrl, setLogoUrl] = useState('');
+    const [pieChartUrl, setPieChartUrl] = useState('');
+    const [radarChartUrl, setRadarChartUrl] = useState('');
+    const [salUrl, setSalUrl] = useState('');
+    const [predUrl, setPredUrl] = useState('');
+    const [mapUrl, setMapUrl] = useState('');
+    const [starUrl, setStarUrl] = useState('');
+    
 
-    const getLogoUrl = (corp_code, company_name) => {
-        const encodedCompanyName = encodeURIComponent(company_name);
-        const url = `https://unicom-s3.s3.eu-north-1.amazonaws.com/%EC%8B%9C%EA%B0%81%ED%99%94/%EC%8B%9C%EA%B0%81%ED%99%94_${encodedCompanyName}/${encodedCompanyName}+%EB%A1%9C%EA%B3%A0.png`;
-        return url;
-    };
+    const fetchImageUrls = async (searchInput) => {
+        try {
+            const responseCorp = await axios.get('https://g39725izal.execute-api.eu-north-1.amazonaws.com/stage1/corp', {
+                params: {
+                    corp_name: searchInput,
+                },
+            });
 
-    const handleSearchSubmit = (event) => {
-        event.preventDefault();
+            console.log(responseCorp);  // Add this line
+            const data = responseCorp.data; // parse the JSON string
 
-        setCorpCode('170558');
-        setCompanyName(searchInput);
-    };
-
-    const handleSearchInputChange = (event) => {
-        setSearchInput(event.target.value);
-    };
-
-    useEffect(() => {
-        if (corp_code && company_name) {
-            setLogoUrl(getLogoUrl(corp_code, company_name));
+            setLogoUrl(data.logo_url);
+            setPieChartUrl(data.pie_chart_url);
+            setRadarChartUrl(data.radar_chart_url);
+            setSalUrl(data.sal_url);
+            setPredUrl(data.pred_url);
+            setMapUrl(data.map_url);
+            setStarUrl(data.star_url)
+            
+        } catch (error) {
+            console.error(`Error fetching image URLs: `, error);
         }
-    }, [corp_code, company_name]);
+    };
 
-    return (        
+    const handleSearchSubmit = (e) => {
+        e.preventDefault();
+
+        fetchImageUrls(searchInput)
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((error) => {
+                console.error(`Error fetching image URLs: `, error);
+            });
+    };
+
+    return (
         <main className="container">
-            <hr/><NavBar/><hr/>
-            <article id="article">                       
+            <hr /><NavBar /><hr />
+            <article id="article">
                 <hgroup>
-                    <br/>
-                    <h1 text align="center">"UniCom!  <kbd>{company_name || '삼성전자'}</kbd> 는 어떤 기업이죠?</h1><br/>
+                    <br />
+                    <h1 text align="center">"UniCom!  <kbd>{searchInput || '삼성전자'}</kbd> 는 어떤 기업이죠?</h1><br />
                     <h2 text align="center">
                         <strong>원하는 기업의 여러 정보를 확인하실 수 있는 페이지에요!</strong>
                     </h2>
                 </hgroup>
                 <div className="grid">
                     <form onSubmit={handleSearchSubmit}>
-                        <input 
-                            type="search" 
-                            id="search" 
-                            name="search" 
-                            placeholder="기업명으로 검색해보세요!" 
+                        <input
+                            type="search"
+                            id="search"
+                            name="search"
+                            placeholder="기업명으로 검색해보세요!"
                             value={searchInput}
-                            onChange={handleSearchInputChange} />
+                            onChange={(e) => setSearchInput(e.target.value)}
+                        />
                         <input type="submit" value="Search" />
                     </form>
                 </div>
-            </article><hr/>
+            </article><hr />
             <div className="grid">
                 <article id="logo">
                     <h4 text align="center"><i>About...</i></h4>
-                    <img src={logoUrl} alt="logo"/>
-                    <footer><small><strong><mark>{company_name || '삼성전자'}</mark></strong>는요...!</small></footer>  
+                    <img src={logoUrl} alt="logo" />
+                    <footer><small><strong><mark>{searchInput || '삼성전자'}</mark></strong>는요...!</small></footer>
                 </article>
-                <article id="wordcloud">
-                    <h4 text align="center"><i>Word Cloud...</i></h4>
-                    <img src='img\wckakao.png' alt="wordcloud"/>
-                    <footer><small><strong><mark>이런 특징</mark></strong>이 있어요!</small></footer>  
+                <article id="piechart">
+                    <h4 text align="center"><i>Pie Chart...</i></h4>
+                    <img src={pieChartUrl} alt="Pie Chart" />
                 </article>
-                <article id="type">
-                    <h4 text align="center"><i>Type...</i></h4>
-                    <img src='img\9.jpg' alt="type"/>
-                    <footer><small><strong><mark>ㅇㅇ유형</mark></strong>으로 분류되었어요!</small></footer>  
-                </article>
-            </div><hr/>
+            </div>
             <div className="grid">
-                <article id="genderratio">
-                    <h4 text align="center"><i>Gender Ratio</i></h4>
-                    <img src='img/sgraph.png' alt="genderratio"/>
-                    <footer><small>구성원의 <strong><mark>남녀성비</mark></strong>는 어떨까요?</small></footer>                  
+                <article id="radar">
+                    <h4 text align="center"><i>Radar Chart...</i></h4>
+                    <img src={radarChartUrl} alt="Radar Chart" />
                 </article>
-                <article id="incomestate">
-                    <h4 text align="center"><i>Income State</i></h4>
-                    <img src='img\incomestate.png' alt="incomestate"/>
-                    <footer><small><strong><mark>재무제표 정보</mark></strong>를 그래프로!</small></footer>                  
+                <article id="sal">
+                    <h4 text align="center"><i>Sal...</i></h4>
+                    <img src={salUrl} alt="Sal" />
                 </article>
-                <article id="Average Salary">
-                    <h4 text align="center"><i>Average Salary</i></h4>
-                    <img src='img\salary.png' alt="salary"/>
-                    <footer><small><strong><mark>평균 연봉</mark></strong>과 업계평균을 비교...?</small></footer>                  
+            </div>
+            <div className="grid">
+                <article id="pred">
+                    <h4 text align="center"><i>Pred...</i></h4>
+                    <img src={predUrl} alt="Pred" />
+                </article>
+                <article id="map">
+                    <h4 text align="center"><i>Map...</i></h4>
+                    <img src={mapUrl} alt="Map" />
+                </article>
+                <article id="star">
+                    <h4 text align="center"><i>Star...</i></h4>
+                    <img src={starUrl} alt="Star" />
                 </article>
             </div>
         </main>
